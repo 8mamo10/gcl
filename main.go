b8fe204159db41f79be36bd17310d37a356d8ff0
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-05-31 23:51:06 +0900

    Add http hundler

--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	var err error
-	_, err = sql.Open("mysql", "root:password@tcp(mysql:3307)/interview_test")
+	_, err = sql.Open("mysql", "root:password@tcp(mysql:3307)/test")
 	if err != nil {
 		panic(err)
 	}
-	println("Hello")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello")
 }
