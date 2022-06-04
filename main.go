f3a616cab75fa0ea57f81debff10db2b74d90321
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-06-04 16:19:46 +0900

    Fetch users from db

--- main.go
+++ main.go
@@ -5,13 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+
 func main() {
 	var err error
-	_, err = sql.Open("mysql", "root:password@tcp(mysql:3307)/mrhc")
+	db, err = sql.Open("mysql", "root:password@tcp(mysql:3306)/mrhc")
 	if err != nil {
 		panic(err)
 	}
@@ -20,5 +23,24 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello")
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		fmt.Fprintf(w, `{"error": "failed to query users: %v"}`, err)
+		return
+	}
+	defer rows.Close()
+
+	var users []string
+	for rows.Next() {
+		var id int
+		var account_id int
+		var name string
+		err := rows.Scan(&id, &account_id, &name)
+		if err != nil {
+			fmt.Fprintf(w, `{"error": "failed to retrieve data: %v"}`, err)
+			return
+		}
+		users = append(users, fmt.Sprintf(`{"id": %d, "account_id: %d, "name": "%s"}`, id, account_id, name))
+	}
+	fmt.Fprintf(w, "["+strings.Join(users, ",")+"]")
 }
