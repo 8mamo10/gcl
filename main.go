5c70019eb187c81c56ededd346aa4acc8d82de35
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-06-09 23:18:08 +0900

    Remove old handler

--- main.go
+++ main.go
@@ -18,8 +18,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	http.HandleFunc("/", handler)
-	//log.Fatal(http.ListenAndServe(":8080", nil))
 	e := echo.New()
 	// e.GET("/users", func(c echo.Context) error {
 	// 	return c.String(http.StatusOK, "Hello Echo")
@@ -52,27 +50,4 @@ func getUsersHandler(ctx echo.Context) error {
 	}
 	ctx.JSON(http.StatusOK, fmt.Sprintf("["+strings.Join(users, ",")+"]"))
 	return err
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	rows, err := db.Query("SELECT * FROM users")
-	if err != nil {
-		fmt.Fprintf(w, `{"error": "failed to query users: %v"}`, err)
-		return
-	}
-	defer rows.Close()
-
-	var users []string
-	for rows.Next() {
-		var id int
-		var account_id int
-		var name string
-		err := rows.Scan(&id, &account_id, &name)
-		if err != nil {
-			fmt.Fprintf(w, `{"error": "failed to retrieve data: %v"}`, err)
-			return
-		}
-		users = append(users, fmt.Sprintf(`{"id": %d, "account_id: %d, "name": "%s"}`, id, account_id, name))
-	}
-	fmt.Fprintf(w, "["+strings.Join(users, ",")+"]")
 }
