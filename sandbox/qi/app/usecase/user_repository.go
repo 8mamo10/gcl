2bc05bbd7fd69eada0e7d8614fbbb9b7a06537b8
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-06-26 22:28:06 +0900

    Add user usecase

---
+++ sandbox/qi/app/usecase/user_repository.go
@@ -0,0 +1,9 @@
+package usecase
+
+import "github.com/8mamo10/gcl/sandbox/qi/app/domain"
+
+type UserRepository interface {
+	Store(domain.User) (int, error)
+	FindById(int) (domain.User, error)
+	FindAll() (domain.Users, error)
+}
